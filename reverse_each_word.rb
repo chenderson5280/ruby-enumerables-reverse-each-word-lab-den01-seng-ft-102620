3064a9744bf0e2e4ad9cb09772bb382e4241c235
Christopher Henderson <chenderson5280@gmail.com>
2020-10-16 02:43:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word (sentence)
 #result = []
 result = ""
 sentence.split.collect do |word|
-result.concat(word.reverse())
+result.concat(word.reverse().join(" "))
 #result.push(word.reverse())
 end
 result
